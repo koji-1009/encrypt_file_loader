5fd2f72f5bf4ba64a9084bcb872e5da65cc036e9
Koji Wakamiya <koji.wakamiya@gmail.com>
2023-04-11 01:00:36 +0900

    Update freezed

--- lib/src/webcrypto/crypto_type.freezed.dart
+++ lib/src/webcrypto/crypto_type.freezed.dart
@@ -1,7 +1,7 @@
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint
-// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
 part of 'crypto_type.dart';
 
@@ -475,6 +475,7 @@ class _$TypeAesCtr with DiagnosticableTreeMixin implements TypeAesCtr {
   final List<int> _counter;
   @override
   List<int> get counter {
+    if (_counter is EqualUnmodifiableListView) return _counter;
     // ignore: implicit_dynamic_type
     return EqualUnmodifiableListView(_counter);
   }
@@ -699,6 +700,7 @@ class _$TypeAesGcm with DiagnosticableTreeMixin implements TypeAesGcm {
   List<int>? get authTag {
     final value = _authTag;
     if (value == null) return null;
+    if (_authTag is EqualUnmodifiableListView) return _authTag;
     // ignore: implicit_dynamic_type
     return EqualUnmodifiableListView(value);
   }
@@ -708,6 +710,7 @@ class _$TypeAesGcm with DiagnosticableTreeMixin implements TypeAesGcm {
   List<int>? get additionalData {
     final value = _additionalData;
     if (value == null) return null;
+    if (_additionalData is EqualUnmodifiableListView) return _additionalData;
     // ignore: implicit_dynamic_type
     return EqualUnmodifiableListView(value);
   }
@@ -919,6 +922,7 @@ class _$TypeRsaOaep with DiagnosticableTreeMixin implements TypeRsaOaep {
   List<int>? get label {
     final value = _label;
     if (value == null) return null;
+    if (_label is EqualUnmodifiableListView) return _label;
     // ignore: implicit_dynamic_type
     return EqualUnmodifiableListView(value);
   }
