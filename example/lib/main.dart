896996d3cc745eea8b9035c8532ed93a1345fd64
Koji Wakamiya <koji.wakamiya@gmail.com>
2023-07-17 14:52:11 +0900

    Update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:encrypt_file_loader/encrypt_file_loader.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,15 +17,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,15 +27,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -48,68 +34,104 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final _db = EncryptFileLoader();
+  String _result = 'no loaded data';
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Text(_result),
+            ElevatedButton(
+              onPressed: () async {
+                final value = await _db.loadAndDecrypt(
+                  url:
+                      'https://raw.githubusercontent.com/koji-1009/encrypt_file_loader/main/fixture/plain.json',
+                  type: const TypePlain(),
+                );
+
+                setState(() {
+                  _result = switch (value) {
+                    LoaderSuccess(
+                      data: final data,
+                    ) =>
+                      utf8.decode(data),
+                    _ => 'no loaded data',
+                  };
+                });
+              },
+              child: const Text('load plain text'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            ElevatedButton(
+              onPressed: () async {
+                final value = _db.loadAndDecrypt(
+                  url:
+                      'https://raw.githubusercontent.com/koji-1009/encrypt_file_loader/main/fixture/aes_cbc.txt',
+                  type: TypeAesCbc(
+                    key: await AesCbcSecretKey.importRawKey(
+                      [
+                        169,
+                        153,
+                        153,
+                        89,
+                        151,
+                        174,
+                        104,
+                        213,
+                        193,
+                        185,
+                        118,
+                        112,
+                        123,
+                        39,
+                        157,
+                        141,
+                      ],
+                    ),
+                    iv: Uint8List.fromList(
+                      [
+                        205,
+                        108,
+                        3,
+                        111,
+                        125,
+                        147,
+                        153,
+                        175,
+                        228,
+                        101,
+                        195,
+                        225,
+                        18,
+                        252,
+                        192,
+                        222,
+                      ],
+                    ),
+                  ),
+                );
+
+                setState(() {
+                  _result = switch (value) {
+                    LoaderSuccess(
+                      data: final data,
+                    ) =>
+                      utf8.decode(data),
+                    _ => 'no loaded data',
+                  };
+                });
+              },
+              child: const Text('load aes cbc'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
